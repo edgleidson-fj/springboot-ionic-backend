b5fec793042ba6a42d7a6342a0787b1d8f017e13
Edgleidson F. de Jesus <wake-up88@hotmail.com>
2021-05-24 19:03:02 -0300

    Aula: 99 Acrescentando mais Produtos para testar INFINITY SCROLL

--- src/main/java/com/edgleidson/cursomc/CursomcApplication.java
+++ src/main/java/com/edgleidson/cursomc/CursomcApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 
 // CursoMC - Curso de Modelagem Conceitual.
 
-// Aula: 98 Padronizando o formato das exceções.
+// Aula: 99 Acrescentando mais Produtos para testar INFINITY SCROLL.
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
